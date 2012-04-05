837254a65e57581833f32fb2c2dbd339c5d22733
Patrick Walton <pcwalton@mimiga.net>
2012-04-04 19:48:53 -0700

    NSApp testcase

--- appkit.rs
+++ appkit.rs
@@ -1,6 +1,14 @@
+import base::objc;
+
 #[nolink]
 #[link_args="-framework AppKit"]
 native mod appkit {
     fn NSBeep();
+}
+
+fn NSApp() -> base::id {
+    let klass = str::as_c_str("NSApplication") { |s| objc::objc_getClass(s) };
+    let sel = str::as_c_str("sharedApplication") { |s| objc::sel_registerName(s) };
+    ret objc::objc_msgSend(klass, sel);
 }
 

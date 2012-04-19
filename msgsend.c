9a43115ca9f5309543f0646252a3d420fdd40628
Brian Anderson <banderson@mozilla.com>
2012-04-18 19:25:01 -0700

    Remove some debug logging

--- msgsend.c
+++ msgsend.c
@@ -3,9 +3,5 @@
 
 void
 msgSend1IdHACK(id theReceiver, SEL theSender, id id) {
-  printf("msgSend1IdHACK\n");
-  printf("%ld\n", theReceiver);
-  printf("%ld\n", theSender);
-  printf("%ld\n", id);
   objc_msgSend(theReceiver, theSender, id);
 }

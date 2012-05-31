478f61bb68feea43c22c9b88f52393ea15908dae
Brian Anderson <banderson@mozilla.com>
2012-05-31 15:26:15 -0700

    Add some CoreGraphics bindings

---
+++ cg.rs
@@ -0,0 +1,19 @@
+// FIXME: Should be bindgenned
+
+import libc::{c_void, c_char, size_t};
+
+#[nolink]
+native mod cg {
+    fn CGDataProviderCreateWithFilename(filename: *c_char) -> CGDataProviderRef;
+    fn CGDataProviderCreateWithData(info: *c_void,
+                                    data: *c_void,
+                                    size: size_t,
+                                    releaseData: CGDataProviderReleaseDataCallback
+                                   ) -> CGDataProviderRef;
+    fn CGDataProviderRelease(provider: CGDataProviderRef);
+    fn CGFontCreateWithDataProvider(provider: CGDataProviderRef) -> CGFontRef;
+}
+
+type CGFontRef = *c_void;
+type CGDataProviderRef = *c_void;
+type CGDataProviderReleaseDataCallback = *c_void;

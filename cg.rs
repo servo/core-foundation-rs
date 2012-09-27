15ff4a2fb4a0cd9ab18c714528eb5d4d65bb8008
Patrick Walton <pcwalton@mimiga.net>
2012-09-27 10:21:52 -0700

    De-export more

--- cg.rs
+++ cg.rs
@@ -4,17 +4,17 @@ use libc::{c_void, c_char, size_t};
 
 #[nolink]
 extern mod cg {
-    fn CGDataProviderCreateWithFilename(filename: *c_char) -> CGDataProviderRef;
-    fn CGDataProviderCreateWithData(info: *c_void,
-                                    data: *c_void,
-                                    size: size_t,
-                                    releaseData: CGDataProviderReleaseDataCallback
-                                   ) -> CGDataProviderRef;
-    fn CGDataProviderRelease(provider: CGDataProviderRef);
-    fn CGFontCreateWithDataProvider(provider: CGDataProviderRef) -> CGFontRef;
-    fn CGFontRelease(font: CGFontRef);
+    pub fn CGDataProviderCreateWithFilename(filename: *c_char) -> CGDataProviderRef;
+    pub fn CGDataProviderCreateWithData(info: *c_void,
+                                        data: *c_void,
+                                        size: size_t,
+                                        releaseData: CGDataProviderReleaseDataCallback
+                                        ) -> CGDataProviderRef;
+    pub fn CGDataProviderRelease(provider: CGDataProviderRef);
+    pub fn CGFontCreateWithDataProvider(provider: CGDataProviderRef) -> CGFontRef;
+    pub fn CGFontRelease(font: CGFontRef);
 }
 
-type CGFontRef = *c_void;
-type CGDataProviderRef = *c_void;
-type CGDataProviderReleaseDataCallback = *c_void;
+pub type CGFontRef = *c_void;
+pub type CGDataProviderRef = *c_void;
+pub type CGDataProviderReleaseDataCallback = *c_void;

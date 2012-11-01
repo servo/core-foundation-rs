2d142a2bafc34553dd669d5d96bcc4e74cc472ef
Brian J. Burg <burg@cs.washington.edu>
2012-11-01 15:12:36 -0700

    Start adding bindings for CTFontDescriptor and CTFontManager.

---
+++ font_manager.rs
@@ -0,0 +1,26 @@
+use cf = core_foundation;
+use cf::array::CFArrayRef;
+use cf::url::CFURLRef;
+
+extern {
+    /*
+     * CTFontManager.h
+     */
+
+    // Incomplete function bindings are mostly related to CoreText font matching, which
+    // we implement in a platform-independent manner using FontMatcher.
+
+    //fn CTFontManagerCompareFontFamilyNames
+    fn CTFontManagerCopyAvailableFontURLs() -> CFArrayRef;
+    fn CTFontManagerCopyAvailableFontFamilyNames() -> CFArrayRef;
+    fn CTFontManagerCopyAvailablePostScriptNames() -> CFArrayRef;
+    fn CTFontManagerCreateFontDescriptorsFromURL(fileURL: CFURLRef) -> CFArrayRef;
+    //fn CTFontManagerCreateFontRequestRunLoopSource
+    //fn CTFontManagerEnableFontDescriptors
+    //fn CTFontManagerGetAutoActivationSetting
+    //fn CTFontManagerGetScopeForURL
+    fn CTFontManagerIsSupportedFont(fontURL: CFURLRef) -> bool;
+    //fn CTFontManagerRegisterFontsForURL
+    //fn CTFontManagerRegisterFontsForURLs
+    //fn 
+}

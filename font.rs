c413a96a8c6b70815b4d5ad7469c7983af417728
Brian J. Burg <burg@cs.washington.edu>
2012-11-16 15:18:10 -0800

    Add binding for CGFont.

--- font.rs
+++ font.rs
@@ -1,12 +1,48 @@
-use libc::{c_void, c_char, size_t};
-use data_provider::CGDataProviderRef;
+use cf = core_foundation;
 
-pub type CGFontRef = *c_void;
+use cf::base::{
+    AbstractCFTypeRef,
+    CFIndex,
+    CFTypeID,
+    CFTypeRef,
+    CFWrapper,
+};
+
+use data_provider::{
+    CGDataProvider,
+    CGDataProviderRef,
+};
+
+use libc::{c_void, c_char, size_t};
+
 pub type CGGlyph = libc::c_ushort;
+
+struct __CGFont { private: () }
+pub type CGFontRef = *__CGFont;
+
+pub impl CGFontRef : AbstractCFTypeRef {
+    pure fn as_type_ref(&self) -> CFTypeRef { *self as CFTypeRef }
+    static pure fn type_id() -> CFTypeID unsafe { CGFontGetTypeID() }
+}
+
+pub type CGFont = CFWrapper<CGFontRef, (), ()>;
+
+pub fn create_with_data_provider(provider: &CGDataProvider) -> CGFont {
+    // TODO: error handling
+    let value = CGFontCreateWithDataProvider(*provider.borrow_ref());
+    CFWrapper::wrap_owned(value)
+}
 
 #[nolink]
 #[link_args="-framework ApplicationServices"]
 extern {
-    pub fn CGFontCreateWithDataProvider(provider: CGDataProviderRef) -> CGFontRef;
-    pub fn CGFontRelease(font: CGFontRef);
+
+    // TODO: basically nothing has bindings (even commented-out)  besides what we use.
+    fn CGFontCreateWithDataProvider(provider: CGDataProviderRef) -> CGFontRef;
+    fn CGFontGetTypeID() -> CFTypeID;
+
+    // These do the same thing as CFRetain/CFRelease, except
+    // gracefully handle a NULL argument. We don't use them.
+    fn CGFontRetain(font: CGFontRef);
+    fn CGFontRelease(font: CGFontRef);
 }

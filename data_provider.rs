adb526a6ca8deee37152ef3ba1197894286e2204
Simon Sapin <simon.sapin@exyr.org>
2013-09-02 17:08:31 +0100

    Upgrade to rustc 0.8-pre (0ac3e02 2013-08-30 05:45:45 -0700)

--- data_provider.rs
+++ data_provider.rs
@@ -32,7 +32,7 @@ impl AbstractCFTypeRef for CGDataProviderRef {
     fn as_type_ref(&self) -> CFTypeRef { *self as CFTypeRef }
 
     #[fixed_stack_segment]
-    fn type_id() -> CFTypeID {
+    fn type_id(_dummy: Option<CGDataProviderRef>) -> CFTypeID {
         unsafe {
             CGDataProviderGetTypeID()
         }

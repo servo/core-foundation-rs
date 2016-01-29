a96019726c177ccb250c4b64e08ff2bf16896af7
hc <henycr@gmail.com>
2016-01-29 12:19:51 -0500

    init status bar button (icon)
    the minimum to get a status bar button (icon) to appear

--- src/appkit.rs
+++ src/appkit.rs
@@ -67,6 +67,7 @@ pub unsafe fn NSApp() -> id {
 #[repr(i64)]
 pub enum NSApplicationActivationPolicy {
     NSApplicationActivationPolicyRegular = 0,
+    NSApplicationActivationPolicyAccessory = 1,
     NSApplicationActivationPolicyERROR = -1
 }
 
@@ -294,7 +295,7 @@ impl NSApplication for id {
     }
 }
 
-pub trait NSRunningApplication { 
+pub trait NSRunningApplication {
     unsafe fn currentApplication(_: Self) -> id {
         msg_send![class("NSRunningApplication"), currentApplication]
     }
@@ -1312,7 +1313,7 @@ pub trait NSOpenGLPixelFormat {
         msg_send![class("NSOpenGLPixelFormat"), alloc]
     }
 
-    // Creating an NSOpenGLPixelFormat Object 
+    // Creating an NSOpenGLPixelFormat Object
 
     unsafe fn initWithAttributes_(self, attributes: &[u32]) -> id;
 
@@ -1324,7 +1325,7 @@ pub trait NSOpenGLPixelFormat {
 }
 
 impl NSOpenGLPixelFormat for id {
-    // Creating an NSOpenGLPixelFormat Object 
+    // Creating an NSOpenGLPixelFormat Object
 
     unsafe fn initWithAttributes_(self, attributes: &[u32]) -> id {
         msg_send![self, initWithAttributes:attributes]
@@ -2303,5 +2304,56 @@ impl NSScreen for id {
 
     unsafe fn convertRectToBacking_(self, aRect: NSRect) -> NSRect {
         msg_send![self, convertRectToBacking:aRect]
+    }
+}
+
+pub trait NSImage {
+    unsafe fn alloc(_: Self) -> id {
+        msg_send![class("NSImage"), alloc]
+    }
+
+    unsafe fn initByReferencingFile_(self, file_path: id /* (NSString *) */) -> id;
+}
+
+impl NSImage for id {
+    unsafe fn initByReferencingFile_(self, file_path: id /* (NSString *) */) -> id {
+        msg_send![self, initByReferencingFile:file_path]
+    }
+}
+
+pub trait NSButton {
+     unsafe fn setImage_(self, img: id /* (NSImage *) */);
+}
+
+impl NSButton for id {
+    unsafe fn setImage_(self, img: id /* (NSImage *) */) {
+        msg_send![self, setImage:img]
+    }
+}
+
+pub trait NSStatusItem {
+    unsafe fn button(self) -> id /* (NSStatusBarButton *) */;
+}
+
+impl NSStatusItem for id {
+    unsafe fn button(self) -> id /* (NSStatusBarButton *) */ {
+        msg_send![self, button]
+    }
+}
+
+pub const NSVariableStatusItemLength: CGFloat = -1.0;
+pub const NSSquareStatusItemLength: CGFloat = -2.0;
+
+pub trait NSStatusBar {
+    unsafe fn systemStatusBar(_: Self) -> id {
+        msg_send![class("NSStatusBar"), systemStatusBar]
+    }
+
+    unsafe fn statusItemWithLength_(self, length: CGFloat) -> id /* (NSStatusItem *) */;
+}
+
+impl NSStatusBar for id {
+    unsafe fn statusItemWithLength_(self, length: CGFloat) -> id /* (NSStatusItem *) */ {
+        msg_send![self, statusItemWithLength:length]
     }
 }

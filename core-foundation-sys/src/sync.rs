f077c50dcf5293e79e84c53ecaced6ed0ecf5ba1
Anthony Ramine <n.oxyde@gmail.com>
2016-10-12 11:17:36 +0200

    Add CFMut<T>
    This type provides DerefMut access to the underlying Core Foundation type.
    Will be used for CFMutableString and friends.

--- core-foundation-sys/src/sync.rs
+++ core-foundation-sys/src/sync.rs
@@ -13,7 +13,8 @@ use base::{CFRelease, CFRetain, CFType};
 use std::cmp::{Eq, Ord, Ordering, PartialEq, PartialOrd};
 use std::fmt;
 use std::hash::{Hash, Hasher};
-use std::ops::Deref;
+use std::mem;
+use std::ops::{Deref, DerefMut};
 
 #[repr(C)]
 pub struct CFRef<T: CFType> { ptr: *const CFShared<T> }
@@ -33,6 +34,16 @@ impl<T: CFType> CFRef<T> {
             Ok(CFRef { ptr: retained })
         } else {
             Err(())
+        }
+    }
+
+    fn into_mut(self) -> Result<CFMut<T>, Self> {
+        if self.as_object().retain_count() == 1 {
+            let result = CFMut { ptr: self.ptr as *const _ as *mut _ };
+            mem::forget(self);
+            Ok(result)
+        } else {
+            Err(self)
         }
     }
 }
@@ -155,3 +166,84 @@ impl<T: CFType + PartialOrd> PartialOrd for CFShared<T> {
 
 unsafe impl<T: CFType + Send> Send for CFShared<T> {}
 unsafe impl<T: CFType + Sync> Sync for CFShared<T> {}
+
+#[repr(C)]
+pub struct CFMut<T: CFType> { ptr: *mut T }
+
+impl<T: CFType> CFMut<T> {
+    #[inline]
+    pub unsafe fn from_retained(retained: *const CFShared<T>) -> Self {
+        assert!(!retained.is_null());
+        assert!((&*retained).as_object().retain_count() == 1);
+        CFMut { ptr: retained as *const _ as *mut _ }
+    }
+
+    fn into_ref(self) -> CFRef<T> {
+        let result = CFRef { ptr: self.ptr as *const _ as *const _ };
+        mem::forget(self);
+        result
+    }
+}
+
+impl<T: CFType> Deref for CFMut<T> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &T {
+        unsafe { &*self.ptr }
+    }
+}
+
+impl<T: CFType> DerefMut for CFMut<T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.ptr }
+    }
+}
+
+impl<T: CFType> Drop for CFMut<T> {
+    #[inline]
+    fn drop(&mut self) {
+        unsafe { CFRelease(self.as_object()) }
+    }
+}
+
+impl<T: CFType + fmt::Debug> fmt::Debug for CFMut<T> {
+    #[inline]
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        (&**self).fmt(formatter)
+    }
+}
+
+impl<T: CFType> Hash for CFMut<T> {
+    #[inline]
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.as_object().hash(state)
+    }
+}
+
+impl<T: CFType + Eq> Eq for CFMut<T> {}
+
+impl<T: CFType + PartialEq> PartialEq for CFMut<T> {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        (&**self).eq(&**other)
+    }
+}
+
+impl<T: CFType + Ord> Ord for CFMut<T> {
+    #[inline]
+    fn cmp(&self, other: &Self) -> Ordering {
+        (&**self).cmp(&**other)
+    }
+}
+
+impl<T: CFType + PartialOrd> PartialOrd for CFMut<T> {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        (&**self).partial_cmp(&**other)
+    }
+}
+
+unsafe impl<T: CFType> Send for CFMut<T> {}
+unsafe impl<T: CFType> Sync for CFMut<T> {}

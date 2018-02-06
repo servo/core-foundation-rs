25ce729f76324af90a9a8366976cf399a468d4f0
Lee Salzman <lsalzman@mozilla.com>
2018-02-06 07:42:06 -0800

    allow creating a CTFont from a CGFont with variations

--- core-text/src/font_descriptor.rs
+++ core-text/src/font_descriptor.rs
@@ -273,6 +273,15 @@ pub fn new_from_attributes(attributes: &CFDictionary<CFType, CFType>) -> CTFontD
     }
 }
 
+pub fn new_from_variations(variations: &CFDictionary<CFType, CFType>) -> CTFontDescriptor {
+    unsafe {
+        let var_key = CFType::wrap_under_get_rule(mem::transmute(kCTFontVariationAttribute));
+        let var_val = CFType::wrap_under_get_rule(variations.as_CFTypeRef());
+        let attributes = CFDictionary::from_CFType_pairs(&[(var_key, var_val)]);
+        new_from_attributes(&attributes)
+    }
+}
+
 pub fn debug_descriptor(desc: &CTFontDescriptor) {
     println!("family: {}", desc.family_name());
     println!("name: {}", desc.font_name());

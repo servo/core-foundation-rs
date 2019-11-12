727837f12e8ba7ce4b86004fbdba2f2dfaf5c61b
Qifan Lu <lqf.1996121@gmail.com>
2019-11-11 20:55:59 -0800

    Use MaybeUninit instead of mem::uninitialized

--- core-foundation/src/url.rs
+++ core-foundation/src/url.rs
@@ -16,9 +16,9 @@ use string::{CFString};
 
 use core_foundation_sys::base::{kCFAllocatorDefault, Boolean};
 use std::fmt;
+use std::mem::MaybeUninit;
 use std::ptr;
 use std::path::{Path, PathBuf};
-use std::mem;
 
 use libc::{c_char, strlen, PATH_MAX};
 
@@ -78,13 +78,15 @@ impl CFURL {
     pub fn to_path(&self) -> Option<PathBuf> {
         // implementing this on Windows is more complicated because of the different OsStr representation
         unsafe {
-            let mut buf: [u8; PATH_MAX as usize] = mem::uninitialized();
-            let result = CFURLGetFileSystemRepresentation(self.0, true as Boolean, buf.as_mut_ptr(), buf.len() as CFIndex);
+            let mut buf = MaybeUninit::<[u8; PATH_MAX as usize]>::uninit();
+            let result = CFURLGetFileSystemRepresentation(self.0, true as Boolean, buf.as_mut_ptr() as *mut u8, PATH_MAX as CFIndex);
             if result == false as Boolean {
                 return None;
             }
-            let len = strlen(buf.as_ptr() as *const c_char);
-            let path = OsStr::from_bytes(&buf[0..len]);
+
+            let buf_init = buf.assume_init();
+            let len = strlen(buf_init.as_ptr() as *const c_char);
+            let path = OsStr::from_bytes(&buf_init[0..len]);
             Some(PathBuf::from(path))
         }
     }

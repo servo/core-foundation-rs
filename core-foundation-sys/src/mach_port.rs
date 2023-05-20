fa3ff5fa99dfcc63f53b234243cfaf28e9ff00ab
michaelwright235 <maiklpolkovnikov@gmail.com>
2023-05-21 00:41:52 +0300

    sys: mach_port.rs: New functions & categorization

--- core-foundation-sys/src/mach_port.rs
+++ core-foundation-sys/src/mach_port.rs
@@ -1,4 +1,5 @@
-pub use base::{CFAllocatorRef, CFIndex, CFTypeID};
+use base::{CFAllocatorRef, CFIndex, CFTypeID, Boolean, mach_port_t};
+use string::CFStringRef;
 use runloop::CFRunLoopSourceRef;
 use std::os::raw::c_void;
 
@@ -6,15 +7,39 @@ use std::os::raw::c_void;
 pub struct __CFMachPort(c_void);
 pub type CFMachPortRef = *const __CFMachPort;
 
+pub type CFMachPortCallBack = extern "C" fn (port: CFMachPortRef, msg: *mut c_void, size: CFIndex, info: *mut c_void);
+pub type CFMachPortInvalidationCallBack = extern "C" fn (port: CFMachPortRef, info: *mut c_void);
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct CFMachPortContext {
+    pub version: CFIndex,
+    pub info: *mut c_void,
+    pub retain: extern "C" fn (info: *const c_void) -> *const c_void,
+    pub release: extern "C" fn (info: *const c_void),
+    pub copyDescription: extern "C" fn (info: *const c_void) -> CFStringRef,
+}
+
 extern "C" {
     /*
      * CFMachPort.h
      */
-    pub fn CFMachPortCreateRunLoopSource(
-        allocator: CFAllocatorRef,
-        port: CFMachPortRef,
-        order: CFIndex,
-    ) -> CFRunLoopSourceRef;
-    
+
+    /* Creating a CFMachPort Object */
+    pub fn CFMachPortCreate(allocator: CFAllocatorRef, callout: CFMachPortCallBack, context: *mut CFMachPortContext, shouldFreeInfo: *mut Boolean) -> CFMachPortRef;
+    pub fn CFMachPortCreateWithPort(allocator: CFAllocatorRef, portNum: mach_port_t, callout: CFMachPortCallBack, context: *mut CFMachPortContext, shouldFreeInfo: *mut Boolean) -> CFMachPortRef;
+
+    /* Configuring a CFMachPort Object */
+    pub fn CFMachPortInvalidate(port: CFMachPortRef);
+    pub fn CFMachPortCreateRunLoopSource(allocator: CFAllocatorRef, port: CFMachPortRef, order: CFIndex) -> CFRunLoopSourceRef;
+    pub fn CFMachPortSetInvalidationCallBack(port: CFMachPortRef, callout: CFMachPortInvalidationCallBack);
+
+    /* Examining a CFMachPort Object */
+    pub fn CFMachPortGetContext(port: CFMachPortRef, context: *mut CFMachPortContext);
+    pub fn CFMachPortGetInvalidationCallBack(port: CFMachPortRef) -> CFMachPortInvalidationCallBack;
+    pub fn CFMachPortGetPort(port: CFMachPortRef) -> mach_port_t;
+    pub fn CFMachPortIsValid(port: CFMachPortRef) -> Boolean;
+
+    /* Getting the CFMachPort Type ID */
     pub fn CFMachPortGetTypeID() -> CFTypeID;
 }

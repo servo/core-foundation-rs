a79a10bd4e934cfcc6e65f9f684dd3f039772a3a
nashaofu <diaocheng@outlook.com>
2024-03-28 23:41:44 +0800

    fix: Fix kCGWindowSharingReadWrite definition error

--- core-graphics/src/window.rs
+++ core-graphics/src/window.rs
@@ -24,7 +24,8 @@ pub type CGWindowID = u32;
 pub type CGWindowSharingType = u32;
 pub const kCGWindowSharingNone: CGWindowSharingType = 0;
 pub const kCGWindowSharingReadOnly: CGWindowSharingType = 1;
-pub const kCGWindowSharingReadWrite: CGWindowSharingType = 1;
+// https://developer.apple.com/documentation/coregraphics/cgwindowsharingtype/kcgwindowsharingreadwrite?language=objc
+pub const kCGWindowSharingReadWrite: CGWindowSharingType = 2;
 
 pub type CGWindowBackingType = u32;
 pub const kCGWindowBackingStoreRetained: CGWindowBackingType = 0;
